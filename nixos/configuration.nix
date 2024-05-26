b3ba566abf71ffd6e75c14f2aad44ce9e84cd82f
jeelpatel231 <jeelpatel231@gmail.com>
2024-05-26 15:52:24 +0530

    the line was already in the config, just commented out

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -113,8 +113,6 @@
 
   programs.adb.enable = true;
 
-  networking.firewall.enable = false;
-
   fonts.packages = with pkgs; [
     (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" "JetBrainsMono" ]; })
   ];
@@ -239,7 +237,7 @@
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
