337ead257f6c2b58b6db0e8bfd88e02ef9edaeb6
jeelpatel231 <jeelpatel231@gmail.com>
2024-05-29 10:24:07 +0530

    allow running generic linux binaries in nixos

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -247,4 +247,14 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
 
+  ### For running generic linux binaries in nix os
+  programs.nix-ld = {
+    enable = true;
+    libraries = with pkgs; [
+      # dynmically linked dependencies for binaries go here
+      stdenv.cc.cc.lib
+      zlib
+    ];
+  };
+
 }
